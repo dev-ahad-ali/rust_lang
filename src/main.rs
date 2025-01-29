8ed2d7646e77d6123249ad59e607f853b53950af
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-30 02:22:26 +0600

    Update: numeric operations

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,27 @@ fn main() {
 
     // --- Scalar types : integers, floating points numbers, Booleans and characters
 
-    // Integer types -->
+    // Integer -->
+
+    // floating points number
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // Numeric operations
+
+    // addition
+    let sum = 5 + 1;
+
+    // subtraction
+    let difference = 5 - 1;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 52.2 / 32.3;
+    let truncated = -5 / 3; // result in -1
+
+    // remainder
+    let remainder = 42 % 5;
 }
