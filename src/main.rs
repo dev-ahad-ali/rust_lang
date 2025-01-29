feffe995120c3371711de68764143f84043250aa
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-30 01:10:43 +0600

    Update: lessons intro created on main branch

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
 fn main() {
-    println!("Guess the number !");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is : {secret_number}"); // remove in production
-
-    loop {
-        println!("Please input your guess !");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed : {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win !");
-                break;
-            }
-        }
-    }
+    // Welcome to Rust Language Book
+    // run git branch to checkout all the lessons
+    println!("Hello Rust 🦀");
 }
