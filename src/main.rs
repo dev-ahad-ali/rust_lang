2fc38bba5fc188758525a3f3c372a0ff7a8aa9af
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-30 01:21:30 +0600

    Update: shadowing example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is : {x}");
+    }
+
+    println!("The value of x is : {x}");
+
+    // shadowing help when we want to change types of the same data, For example say our program asks a user to show how many spaces they want between some text by inputting space characters, and then we want to store that input as a number
+
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
