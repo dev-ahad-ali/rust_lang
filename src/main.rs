9c4493b6dc64a4995ad5d2619f2f0ca5bd1aa8b1
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-30 17:56:02 +0600

    Update: functions noted

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    another_function(3);
+    print_labeled_measurement(5, 'h');
+
+    // this is an example of expression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is : {y}");
+
+    let z = five();
+    println!("The value of z is : {z}");
+
+    let p = plus_one(5);
+    println!("The value of p is : {p}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is : {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is : {value}{unit_label}");
+}
+
+// line without semicolon ; (expression) will be the return value of the function or return key can be used for explicitly return a value
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
