3cffd83fa66cee7dcfc80fb5d5993b39b28c1583
dev-ahad-ali <ahadali.dev@gmail.com>
2025-02-21 23:40:01 +0600

    References and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let s = String::from("hello");
+    let mut s1 = String::from("Hello");
+    let mut r = String::from("Ref");
+
+    let length = calculate_length(&s);
+
+    println!("The size of {s} is {length}");
+
+    change(&mut s1);
+
+    // simultaneous mutable reference are not allowed
+    // let r1 = &mut r;
+    // let r2 = &mut r;
+
+    {
+        let r1 = &mut r;
+        println!("{}", r1);
+    } // r1 goes out of scope here, so we can make a new reference with no problems. allowing for multiple mutable references, just not simultaneous ones
+
+    let r2 = &mut r;
+    println!("{}", r2);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", World");
 }
