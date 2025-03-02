53ba28f753c6acc35f48dee653be6becbb2595fe
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-02 15:29:29 +0600

    iter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let s: String = String::from("Hello World");
+
+    let word = first_word(&s);
+
+    println!("{word}");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
