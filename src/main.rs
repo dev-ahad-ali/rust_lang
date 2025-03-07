ae3cd8926fb0dca153bb334dab1d1301cc786fad
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-07 14:47:16 +0600

    more about string slice

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 fn main() {
     let s: String = String::from("Hello World");
+    let s1 = "hello world"; // as string slice
 
     let word = first_word(&s);
+    let word2 = first_word(s1);
 
-    println!("{word}");
+    println!("The first word is : {word}");
+    println!("The first word is : {word2}");
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
+    // taking &str string slice type makes it more flexible allowing to take
+    // both String and string slice as parameter.
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    s
 }
