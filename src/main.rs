ef804ce64b4a6d8a0ddea23f0977bdff0a19d721
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-07 14:56:06 +0600

    other slices

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ fn main() {
 
     println!("The first word is : {word}");
     println!("The first word is : {word2}");
+
+    // other slices
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]); // Asserts that two expressions are equal to each other.
 }
 
 fn first_word(s: &str) -> &str {
