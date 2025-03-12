5cc420fe1d47ba5903510454242251792927a9cf
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-12 15:09:40 +0600

    refactoring with tuple

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 // calculate the area of a rectangle
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "The area of the rectangle is {} square pixels",
-        area(width1, height1)
-    )
+    let rec1 = (30, 50);
+    println!("The area of the rectangle is {} square pixels", area(rec1))
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
