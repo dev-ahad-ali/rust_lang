796b83b1d7ff6a21eaa5ba0c1fb4890874d1bb7f
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-12 15:54:53 +0600

    debug and printing methods

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 // calculate the area of a rectangle
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 fn main() {
+    let scale = 2;
     let rec1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
-
+    dbg!(&rec1);
+    println!("rec1 is {rec1:#?}");
     println!("The area of the rectangle is {} square pixels", area(&rec1))
 }
 
