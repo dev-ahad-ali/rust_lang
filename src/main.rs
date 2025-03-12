d3bf36c9305547b3f831e21a1a303447f612d354
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-12 14:46:04 +0600

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u64,
+    }
+
+    let mut user1 = User {
+        active: true,
+        email: String::from("some@example.com"),
+        username: String::from("username123"),
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        // active: user1.active,
+        // username: user1.username,
+        // sign_in_count: user1.sign_in_count,
+        email: String::from("new@email.com"),
+        ..user1
+    };
+
+    user1.email = String::from("another@email.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
