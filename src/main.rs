ef9b26ecad0aecc75481f237a9b3d3ca3ae5d9ba
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-12 15:07:37 +0600

    the normal way

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+// calculate the area of a rectangle
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area(width1, height1)
+    )
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
