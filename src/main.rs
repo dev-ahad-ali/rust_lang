6b1d04300ee15248d3b25275f816f42008a34a4c
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-12 16:35:04 +0600

    associated functions

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,48 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
+
+//  valid impl as well
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rec1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let sqr1 = Rectangle::square(20);
 
     if rec1.width() {
         println!("The rectangle has nonzero width, it is {}", rec1.width);
     }
 
-    println!("The area of the rectangle is {} square pixels", rec1.area())
+    println!("The area of the rectangle is {} square pixels", rec1.area());
+    println!("Can rect1 hold rect2? {}", rec1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rec1.can_hold(&rect3));
+    println!("The square {sqr1:#?}");
 }
