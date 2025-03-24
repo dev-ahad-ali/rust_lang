bc85d13cec333f4c3bc213269470746442dc5eaa
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-24 16:03:59 +0600

    match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Alabama,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
 }
