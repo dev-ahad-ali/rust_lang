908a547550b766581bc4492406270eefeba589f7
dev-ahad-ali <ahadali.dev@gmail.com>
2025-03-24 15:19:28 +0600

    add methods to enum

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@ enum IpAddrKind {
     V4,
     V6,
 }
+enum IpAddrString {
+    V4(String),
+    V6(String),
+}
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+impl Message {
+    fn call(&self) {
+        println!("Message enum method");
+    }
+}
 
 struct IpAddr {
     kind: IpAddrKind,
@@ -18,6 +33,8 @@ fn main() {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
+    let m = Message::Write(String::from("Hello Message"));
+    m.call();
 }
 
 fn route(ip_kind: IpAddrKind) {}
