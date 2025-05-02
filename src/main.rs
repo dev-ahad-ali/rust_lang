43a509df368be2fc5ec2b4ad321269db99d7dc7a
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-02 17:48:18 +0600

    error with match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+#![allow(unused)]
+use ::std::fs::File;
+use core::panic;
+use std::io::ErrorKind;
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    // panic!("crash and burn");
+
+    // let v = vec![1, 2, 3];
+
+    // v[99];
+
+    // let greeting_file_result = File::open("hello.txt");
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(error) => panic!("Problem creating the file: {:?}", error),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
 }
