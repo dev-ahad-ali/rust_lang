2cc9bd5395737c3af365d527a6edbe7a99216bb5
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-09 01:06:02 +0600

    implementing methods with trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -105,3 +105,24 @@ pub fn returns_summarizable(switch: bool) -> impl Summary {
         }
     }
 }
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest number is x : {}", self.x);
+        } else {
+            println!("The largest number is y : {}", self.y);
+        }
+    }
+}
