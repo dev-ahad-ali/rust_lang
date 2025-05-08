a4e04bc8e75642e675dad2db74cc76a6eb6587e2
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-08 15:25:49 +0600

    generic types mixing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,34 @@ struct Point<T> {
     y: T,
 }
 
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+impl Point<f32> {
+    fn distance_from_origin(&self) {
+        (self.x.powi(2) + self.y.powi(2)).sqrt();
+    }
+}
+
 struct MultipleTypePoint<T, U> {
     x: T,
     y: U,
+}
+
+struct OtherPoint<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> OtherPoint<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: OtherPoint<X2, Y2>) -> OtherPoint<X1, Y2> {
+        OtherPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn main() {
@@ -18,14 +43,20 @@ fn main() {
     let largest_number = largest(&char_list);
     println!("the largest number is {largest_number}");
 
-    // in struct definitions
+    // in struct definitios
     let integer = Point { x: 1, y: 2 };
     let float = Point { x: 1.0, y: 2.0 };
     let wont_work = Point { x: 1, y: 2.0 };
+    let p = Point { x: 4, y: 2 };
+    println!("p.x = {}", p.x());
 
     let both_integer = MultipleTypePoint { x: 1, y: 2 };
     let both_float = MultipleTypePoint { x: 1.0, y: 2.0 };
     let integer_and_float = MultipleTypePoint { x: 1, y: 2.3 };
+
+    let p1 = OtherPoint { x: 4, y: 2.1 };
+    let p2 = OtherPoint { x: "hello", y: "r" };
+    let p3 = p1.mixup(p2);
 }
 fn largest<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
