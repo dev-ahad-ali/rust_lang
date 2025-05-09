af45dc3708b69f4f21fb6cb4d43acbf579f6d209
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-09 09:23:01 +0600

    liftimes in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+#[allow(unused)]
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let r;
+    {
+        let x = 5;
+        r = &x;
+    }
+    println!("r : {r}");
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+
+    println!("The longest string is {result}");
+}
+
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
