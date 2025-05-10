8d51f4968b78578674a75da7e8790e0c4e5fab94
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-10 13:16:58 +0600

    generic types trait and lifetime together

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use ::std::fmt::Display;
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let string1 = "I'm the longest string";
+    let string2 = "I'm not";
+    let announce = "Let's begin";
+
+    longest_announcement(string1, string2, announce);
+}
+
+fn longest_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    print!("Announcement: {ann}");
+    if x.len() >= y.len() {
+        x
+    } else {
+        y
+    }
 }
