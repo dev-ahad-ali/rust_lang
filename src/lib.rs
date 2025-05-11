bde24ea7ee359625a6c83358f340ded70ef82f83
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-11 18:46:17 +0600

    basic test template

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+/* pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+} */
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 5,
+        };
+
+        let smaller = Rectangle {
+            width: 3,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+        assert!(!smaller.can_hold(&larger));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,1 @@
-fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
-}
+fn main() {}
