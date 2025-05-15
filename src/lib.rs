af411981330761cb2657b84008d32aa0de34a91a
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-15 23:30:51 +0600

    normal tests

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,18 @@ mod test {
         };
 
         assert!(larger.can_hold(&smaller));
-        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(smaller.can_hold(&larger));
     }
 }
