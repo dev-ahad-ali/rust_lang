bb452372feba08cba88e6b878e19dc38f8f7a98f
dev-ahad-ali <ahadali.dev@gmail.com>
2025-05-24 22:39:06 +0600

    drop trait and deref coercion of smart pointers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,59 @@
+use std::ops::Deref;
+
+use crate::List::{Cons, Nil};
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Drop CustomSmartPointer with data : {}", self.data);
+    }
+}
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+    let y2 = Box::new(x); // copied the value of x on the heap
+    let y3 = MyBox::new(x);
+    let m = MyBox::new(String::from("world"));
+    let c = CustomSmartPointer {
+        data: String::from("some stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("more stuff"),
+    };
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *y2);
+    assert_eq!(5, *y3);
+    hello(&m);
+    println!("CustomSmartPointer created with : {:?}, {:?}", c, d);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}");
 }
