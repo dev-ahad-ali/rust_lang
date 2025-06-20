4321d552f0cb40b5d32e536932b4cccf0f7be33f
dev-ahad-ali <ahadali.dev@gmail.com>
2025-06-20 22:34:13 +0600

    joining mutiple futures for async concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,52 @@
+use std::time::Duration;
+
+use ::trpl;
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    trpl::run(async {
+        // let (tx, mut rx) = trpl::channel();
+
+        // let val = String::from("hi");
+        // tx.send(val).unwrap();
+
+        // let received = rx.recv().await.unwrap();
+        // println!("Got: {received}");
+
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx_fut1 = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received: {value}");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join3(tx_fut1, tx_fut, rx_fut).await;
+    })
 }
