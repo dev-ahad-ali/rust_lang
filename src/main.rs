12fe5649fef6a492647538a387807e7be585aa52
dev-ahad-ali <ahadali.dev@gmail.com>
2025-06-21 16:01:42 +0600

    racing furtures

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::time::Duration;
+
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    trpl::run(async {
+        let slow = async {
+            println!("Slow started");
+            trpl::sleep(Duration::from_millis(100)).await;
+            println!("Slow ended");
+        };
+        let fast = async {
+            println!("Fast started");
+            trpl::sleep(Duration::from_millis(50)).await;
+            println!("Fast ended");
+        };
+
+        trpl::race(slow, fast).await;
+    });
 }
