2994642fabc82885a75750c89087ce22c92d9079
dev-ahad-ali <ahadali.dev@gmail.com>
2025-06-23 13:23:07 +0600

    testing sleep and yield

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("{name} ran for {ms} ms");
+}
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    trpl::run(async {
+        let one_ms = Duration::from_millis(1);
+
+        let a = async {
+            println!("'a' started");
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished");
+        };
+
+        let b = async {
+            println!("'b' started.");
+            slow("b", 75);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 15);
+            trpl::yield_now().await;
+            slow("b", 350);
+            trpl::yield_now().await;
+            println!("'b' finished.");
+        };
+
+        trpl::race(a, b).await;
+
+        // testing speed between yield and sleep in futures
+
+        let one_ns = Duration::from_nanos(1);
+
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::sleep(one_ns).await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'sleep' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
+
+        let start = Instant::now();
+        async {
+            for _ in 1..1000 {
+                trpl::yield_now().await;
+            }
+        }
+        .await;
+        let time = Instant::now() - start;
+        println!(
+            "'yield' version finished after {} seconds.",
+            time.as_secs_f32()
+        );
+    });
 }
