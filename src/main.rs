4427038d70127920f02583dce4f515c3f5d3929a
dev-ahad-ali <ahadali.dev@gmail.com>
2025-06-24 15:33:21 +0600

    making async timeout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::{future::Future, time::Duration};
+
+use trpl::Either;
+
+async fn timeout<F: Future>(future_to_try: F, max_time: Duration) -> Result<F::Output, Duration> {
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    trpl::run(async {
+        let slow = async {
+            trpl::sleep(Duration::from_millis(100)).await;
+            "I finished !"
+        };
+
+        match timeout(slow, Duration::from_millis(10)).await {
+            Ok(message) => println!("Succeeded with {message}"),
+            Err(duration) => println!("Failed after {} seconds", duration.as_secs()),
+        }
+    });
 }
