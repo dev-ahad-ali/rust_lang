9326fabd8daa94e46f49580309a87eacc0cf770c
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-01 23:00:23 +0600

    destructuring structs

--- src/main.rs
+++ src/main.rs
@@ -41,4 +41,26 @@ fn main() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    // destructuring structs
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => println!("On neither axis : ({x}, {y})"),
+    }
 }
