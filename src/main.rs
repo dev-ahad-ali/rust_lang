1f930370a4ed484a84784ca3803bd9f14a84c627
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-01 22:36:39 +0600

    matching named variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
     // matching literals
-    let x = 1;
-    match x {
+    let fo = 1;
+    match fo {
         1 => println!("one"),
         2 => println!("two"),
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // matching named variables
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end , x={x:?} and y={y}");
 }
