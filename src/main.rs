4fa20de735fb8cc0a5b72e4e8ef0ecfe93280484
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-01 21:12:37 +0600

    if let and while let patterns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 fn main() {
-    // Welcome to Rust Language Book
-    // run git branch to checkout all the lessons
-    println!("Hello Rust 🦀");
+    // conditional if let expressions
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as background color");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as background color");
+        } else {
+            println!("Using orange as background color");
+        }
+    } else {
+        println!("Using blue as background color");
+    }
+
+    // while let conditional loop
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    std::thread::spawn(move || {
+        for val in [1, 2, 3] {
+            tx.send(val).unwrap();
+        }
+    });
+
+    while let Ok(value) = rx.recv() {
+        println!("{value}");
+    }
 }
