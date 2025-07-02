dae9eb819810254281a08de18a9b0fd987552481
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-02 17:01:38 +0600

    ignoring values

--- src/main.rs
+++ src/main.rs
@@ -99,4 +99,52 @@ fn main() {
 
     // Destructuring structs and tuples
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    // Ignoring values in a pattern
+    fn fo_fn(_: i32, y: i32) {
+        println!("This function only uses y parameter : {y}");
+    }
+    fo_fn(3, 8);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Cannot overwrite an existing customized value");
+        }
+        _ => setting_value = new_setting_value,
+    }
+
+    let numbers = (1, 2, 3, 4, 5);
+    match numbers {
+        (first, _, third, _, fifth) => println!("Some numbers: {first}, {third}, {fifth}"),
+    }
+
+    let _fo3 = 3;
+    let fo4 = 4;
+
+    let s = Some(String::from("hi"));
+
+    // if let Some(_s) = s {
+    //     println!("Found String");
+    // }
+
+    if let Some(_) = s {
+        println!("Found String");
+    }
+
+    println!("{s:?}");
+
+    struct Point3D {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point3D { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point3D { x, .. } => println!("x is : {x}"),
+    }
 }
