ed7abcf5bdd1d82d27ef227127717fff4f8b7ce9
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-02 12:37:59 +0600

    destructuring nested structs and enums

--- src/main.rs
+++ src/main.rs
@@ -63,4 +63,37 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {y}"),
         Point { x, y } => println!("On neither axis : ({x}, {y})"),
     }
+
+    // destructuring nested structs and enums
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("The text message: {text}");
+        }
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r} blue {b} green {g}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h} saturation {s} value {v}");
+        } // _ => (),
+    }
 }
