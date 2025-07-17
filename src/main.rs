0bd45e180c3bf775283f08eb0511f7ffed19fff1
dev-ahad-ali <ahadali.dev@gmail.com>
2025-07-17 22:42:17 +0600

    Disambiguating between methods with the same name

--- src/main.rs
+++ src/main.rs
@@ -49,4 +49,59 @@ fn main() {
             Millimeters(self.0 + (other.0 * 1000))
         }
     }
+
+    // Disambiguating between methods with the same name
+    trait Pilot {
+        fn fly(&self);
+    }
+
+    trait Wizard {
+        fn fly(&self);
+    }
+
+    struct Human;
+
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("This is your captain speaking.");
+        }
+    }
+
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("Up!");
+        }
+    }
+
+    impl Human {
+        fn fly(&self) {
+            println!("*waving arms furiously*");
+        }
+    }
+
+    let person = Human;
+    person.fly();
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+
+    trait Animal {
+        fn baby_name() -> String;
+    }
+
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("Puppy")
+        }
+    }
+
+    println!("A baby dog is called: {}", Dog::baby_name());
+    println!("A baby dog is called: {}", <Dog as Animal>::baby_name());
 }
